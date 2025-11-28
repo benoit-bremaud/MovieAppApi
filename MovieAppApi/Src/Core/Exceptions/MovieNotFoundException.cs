4fd18dcedd3ccb98f52ba1efee35e5c0fda8a682
benoit-bremaud <benoit.bremaud@ynov.com>
2025-11-28 16:08:26 +0100

    docs: add XML documentation to MovieNotFoundException
    - Document custom exception class and use cases
    - Document both constructors (with/without inner exception)
    - Reduce CS1591 warnings from 38 to 36

--- MovieAppApi/Src/Core/Exceptions/MovieNotFoundException.cs
+++ MovieAppApi/Src/Core/Exceptions/MovieNotFoundException.cs
@@ -1,8 +1,22 @@
 namespace MovieAppApi.Src.Core.Exceptions;
 
+/// <summary>
+/// Exception thrown when a requested movie cannot be found in the TMDB API or database.
+/// Used by movie services and controllers to indicate that a movie lookup operation failed.
+/// </summary>
 public class MovieNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MovieNotFoundException"/> class.
+    /// </summary>
+    /// <param name="message">The error message describing why the movie was not found.</param>
     public MovieNotFoundException(string message) : base(message) { }
-    public MovieNotFoundException(string message, Exception innerException) 
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MovieNotFoundException"/> class with an inner exception.
+    /// </summary>
+    /// <param name="message">The error message describing why the movie was not found.</param>
+    /// <param name="innerException">The inner exception that caused this exception.</param>
+    public MovieNotFoundException(string message, Exception innerException)
         : base(message, innerException) { }
 }
