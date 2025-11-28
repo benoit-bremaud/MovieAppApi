92930ff6c3611a75333e2b04d74a95ec615c2ef8
benoit-bremaud <benoit.bremaud@ynov.com>
2025-11-28 16:20:20 +0100

    docs: add XML documentation to AddMovieToPlaylistRequestDto
    - Document DTO class for movie addition operations
    - Document TmdbId property for movie identification
    - Document Title property for display purposes
    - Reduce CS1591 warnings from 18 to 15

--- MovieAppApi/Src/Views/DTO/Playlists/AddMovieToPlaylistRequestDto.cs
+++ MovieAppApi/Src/Views/DTO/Playlists/AddMovieToPlaylistRequestDto.cs
@@ -2,11 +2,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MovieAppApi.Src.Views.DTO.Playlists;
 
+/// <summary>
+/// Data Transfer Object for adding a movie to a playlist.
+/// Contains the required TMDB ID and movie title for the addition operation.
+/// </summary>
 public class AddMovieToPlaylistRequestDto
 {
+    /// <summary>
+    /// Gets or sets the TMDB ID of the movie to add to the playlist.
+    /// This is a required field used to identify the movie in TMDB.
+    /// </summary>
     [Required]
     public int TmdbId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the title of the movie to add to the playlist.
+    /// This is a required field for display purposes.
+    /// </summary>
     [Required]
     public required string Title { get; set; }
 }
