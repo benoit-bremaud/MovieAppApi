2a604ddd6d1367a37379ea02a737dbf9f896fe92
benoit-bremaud <benoit.bremaud@ynov.com>
2025-11-28 16:09:44 +0100

    docs: add XML documentation to PlaylistNotFoundException
    - Document custom exception class for missing playlists
    - Document both constructors (with/without inner exception)
    - Reduce CS1591 warnings from 36 to 34

--- MovieAppApi/Src/Core/Exceptions/PlaylistNotFoundException.cs
+++ MovieAppApi/Src/Core/Exceptions/PlaylistNotFoundException.cs
@@ -1,8 +1,22 @@
 namespace MovieAppApi.Src.Core.Exceptions;
 
+/// <summary>
+/// Exception thrown when a requested playlist cannot be found in the database.
+/// Used by playlist repository and controllers to indicate that a playlist lookup operation failed.
+/// </summary>
 public class PlaylistNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlaylistNotFoundException"/> class.
+    /// </summary>
+    /// <param name="message">The error message describing why the playlist was not found.</param>
     public PlaylistNotFoundException(string message) : base(message) { }
-    public PlaylistNotFoundException(string message, Exception innerException) 
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlaylistNotFoundException"/> class with an inner exception.
+    /// </summary>
+    /// <param name="message">The error message describing why the playlist was not found.</param>
+    /// <param name="innerException">The inner exception that caused this exception.</param>
+    public PlaylistNotFoundException(string message, Exception innerException)
         : base(message, innerException) { }
 }
